6586295120f1fcd3633138b128ee806103bc6eeb
Harshavardhanlav <hprabandhakavi@gmail.com>
2026-09-23 14:41:52 +0530

    Time: 3 ms (63.88%) | Memory: 12.5 MB (32.24%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,5 +1,20 @@
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-     for ch in t:
-       if t.count(ch)>s.count(ch):
-         return ch
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        d_t={}
+        for i in range(0,len(t)):
+            if t[i] in d_t:
+                f=d_t[t[i]]
+                d_t[t[i]]=f+1
+            else:
+                d_t[t[i]]=1
+        for i in range(0,len(s)):
+            f=d_t[s[i]]
+            d_t[s[i]]=f-1
+        for i in d_t.keys():
+            if d_t[i]==1:
+                return i       
